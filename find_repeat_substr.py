17ea1058e3eae3cbc06fd027723ed5d7b3b9e3f0
ashishbunkar0785 <78388214+ashishbunkar0785@users.noreply.github.com>
2023-08-21 08:32:27 +0530

    Update find_repeat_substr.py

--- find_repeat_substr.py
+++ find_repeat_substr.py
@@ -11,32 +11,35 @@ Explanation: It is the substring "ab" twice.
 https://leetcode.com/problems/repeated-substring-pattern/
 */
 
-def repeatedSubstringPattern(self, s):
-    """
-    :type s: str
-    :rtype: bool
-    """
-    temp_str=""
-    return_flag=False
-    for char in s:
-      print char
-      temp_str=temp_str + char
-      print temp_str
-      index = s.index(char)
-      print(s[index+1:(index + 1 + len(temp_str))])
-      if temp_str == s[index+1:(index + 1 + len(temp_str))]:
-         return_flag=True
-         for i in range(index+len(temp_str)+1,len(s)-1,len(temp_str)):
-            print("in if:", i)
-            print(s[i:i+len(temp_str)])
-            if temp_str == s[i:i+len(temp_str)]:
-               return_flag=True
-               continue
-            else:
-               return_flag=False
-               temp_str = ""
-               break
-      if return_flag == True:
-         return True
-      print "------------------------------------------------------"
-  return return_flag
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        temp_str=""
+        return_flag=False
+        for index in range(0,len(s)):
+            char = s[index]
+            #print char
+            temp_str=temp_str + char
+            print temp_str
+            print(s[index+1:(index + 1 + len(temp_str))])
+            if temp_str == s[index+1:(index + 1 + len(temp_str))]:
+                return_flag=True
+                for i in range(index+len(temp_str)+1,len(s),len(temp_str)):
+                    print("in if:", i)
+                    print(s[i:i+len(temp_str)])
+                    if temp_str == s[i:i+len(temp_str)]:
+                        return_flag=True
+                        #print(s[i+len(temp_str)])
+                        continue
+                    else:
+                        return_flag=False
+                        #temp_str = ""
+                        break
+                #if len(s) > (index + len(temp_str)) and len
+            if return_flag == True:
+               return True
+            print "------------------------------------------------------"
+        return return_flag
